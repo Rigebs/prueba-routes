8d29d81cc540193065af40a046dfd48c992d5fe2
Rigebs <165223164+Rigebs@users.noreply.github.com>
2024-06-15 17:35:10 -0500

    Update App.jsx

--- react-router-protected-master/src/App.jsx
+++ react-router-protected-master/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { Admin, Analytics, Dashboard, Home, Landing } from "./pages";
-import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -10,76 +9,58 @@ function App() {
     setUser({
       id: 1,
       name: "John",
-      permissions: ["analize"],
+      permissions: ["analyze"],
       roles: ["admin"],
     });
+
   const logout = () => setUser(null);
 
   return (
     <BrowserRouter>
-      <Navigation />
-
-      {user ? (
-        <button onClick={logout}>Logout</button>
-      ) : (
-        <button onClick={login}>Login</button>
-      )}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/landing">Landing</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          {user && (
+            <>
+              <li>
+                <Link to="/analytics">Analytics</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
 
       <Routes>
-        <Route index element={<Landing />} />
         <Route path="/landing" element={<Landing />} />
-        <Route element={<ProtectedRoute isAllowed={!!user} />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route
-          path="/analytics"
-          element={
-            <ProtectedRoute
-              redirectTo="/home"
-              isAllowed={!!user && user.permissions.includes("analize")}
-            >
-              <Analytics />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute
-              redirectTo="/home"
-              isAllowed={!!user && user.roles.includes("admin")}
-            >
-              <Admin />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {user && (
+          <>
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/admin" element={<Admin />} />
+          </>
+        )}
       </Routes>
-    </BrowserRouter>
-  );
-}
 
-function Navigation() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/landing">Landing</Link>
-        </li>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/analytics">Analytics</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin</Link>
-        </li>
-      </ul>
-    </nav>
+      <div>
+        {user ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <button onClick={login}>Login</button>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
