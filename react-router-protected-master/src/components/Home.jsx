485121d4d221d90ef3aaabecbac3c53c90faa2d8
Rigebs <165223164+Rigebs@users.noreply.github.com>
2024-06-15 18:07:17 -0500

    Create Home.jsx

---
+++ react-router-protected-master/src/components/Home.jsx
@@ -0,0 +1,15 @@
+// Importa React y cualquier otra dependencia necesaria
+import React from 'react';
+
+// Define el componente Home
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la Página de Inicio</h1>
+      <p>Este es un ejemplo de página de inicio.</p>
+    </div>
+  );
+}
+
+// Exporta el componente para poder usarlo en otras partes de tu aplicación
+export default Home;
